4efd76091edbc20e5befc194b8c89f51a832f890
WellisonSilvaa <wonsilva100@gmail.com>
2022-09-25 11:45:20 -0300

    primeiro versão do APK

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { setInterval } from 'timers';
 
 @Component({
   selector: 'app-tab1',
@@ -8,18 +7,42 @@ import { setInterval } from 'timers';
 })
 export class Tab1Page {
 
-  quantidadePaginas;
+  public quantidadePaginas: number;
   public cronometro: any;
-  public tempo: any;
+  public tempo: number;
+  public cronIniciado = false;
+  public resultado: any;
 
-  constructor() {}
-
-  iniciar(){
-    this.tempo = 0;
-    this.cronometro = setInterval(() => {
-      this.tempo = this.tempo + 1;
-      console.log(this.tempo);
-    }, 1000);
+  constructor() {
+    this.resultado = null;
   }
 
+    start(){
+      if(this.quantidadePaginas > 0 && this.quantidadePaginas != null){
+      this.cronIniciado = true;
+      this.tempo = 0;
+      this.cronometro = setInterval(() => {
+        this.tempo ++;
+      }, 1000);
+    }else{
+      alert('Informe um números de páginas');
+    }
+    }
+    stop(){
+      this.cronIniciado = false;
+      clearInterval(this.cronometro);
+
+      this.mostrarResult();
+    }
+    mostrarResult(){
+      this.tempo = this.tempo * this.quantidadePaginas;
+      this.resultado = new Date(this.tempo * 1000).toISOString().substr(11, 8);
+    }
+
+    reiniciar(){
+      this.quantidadePaginas = null;
+      this.tempo = 0;
+      this.cronIniciado =false;
+      this.resultado = null;
+    }
 }
